60f44952cc215a8b47cb6b9995850e01ac4ed3ea
Tasit.s <tasit.s@arise.tech>
2024-05-10 15:19:33 +0700

    [Jet] completed allergies

--- allergies/allergies.ts
+++ allergies/allergies.ts
@@ -1,13 +1,44 @@
+const allergens: any = {
+  1: "eggs",
+  2: "peanuts",
+  4: "shellfish",
+  8: "strawberries",
+  16: "tomatoes",
+  32: "chocolate",
+  64: "pollen",
+  128: "cats",
+};
+
 export class Allergies {
-  constructor(allergenIndex: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  private allergics: string[] = [];
+
+  private findScoreBase(index: number): number {
+    let scoreBase = 1;
+    while (scoreBase * 2 <= index) {
+      scoreBase = scoreBase * 2;
+    }
+    return scoreBase;
+  }
+
+  constructor(allergenIndex: number) {
+    let scoreBase = this.findScoreBase(allergenIndex);
+
+    let score = allergenIndex;
+    while (scoreBase >= 1 && score !== 0) {
+      if (allergens[scoreBase] !== undefined && scoreBase <= score) {
+        this.allergics.push(allergens[scoreBase]);
+      }
+
+      score = score - scoreBase;
+      scoreBase = this.findScoreBase(score);
+    }
   }
 
   public list(): unknown {
-    throw new Error('Remove this statement and implement this function')
+    return this.allergics.reverse();
   }
 
-  public allergicTo(allergen: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public allergicTo(allergen: string): unknown {
+    return this.allergics.includes(allergen);
   }
 }
